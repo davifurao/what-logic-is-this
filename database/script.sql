9ee94b4b0f36f3868b3a3632e498fdd19839e287
Davi Luna <Sdavi738@gmail.com>
2022-07-24 03:07:23 -0300

    database inicial

---
+++ database/script.sql
@@ -0,0 +1,6 @@
+create table usuario(
+nome_usuario varchar(255) not null,
+senha_usuario varchar(255) not null,
+id_usuario int auto_increment not null,
+constraint id_pk primary key (id_usuario)
+);
